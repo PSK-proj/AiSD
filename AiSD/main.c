1ce25e943043c24b76c09a2404e1d40fa877d936
Piotr Miernik <piotrmiernik255@gmail.com>
2022-04-28 01:50:14 +0200

    Add data saving to file

--- AiSD/main.c
+++ AiSD/main.c
@@ -43,7 +43,7 @@ int auto_data_load(int n, int* p, int* q)
         return 1;
     return 0;
 }
-
+// A - czasy
 void stress_test(int n, int* p, int* q, unsigned int iterations, double* avg_bubble, double* avg_quick)
 {
     int* p_copy = (int*)malloc(n * sizeof(int));
@@ -126,6 +126,59 @@ void load_iterations(int* iterations)
     } while (temp <= 0);
     *iterations = temp;
 }
+// A - plik
+void file_handling(int n, int* p, int* q, unsigned short mode, unsigned short* f_w_c, unsigned short* f_d_c, double avg_b, double avg_q)
+{
+    FILE* fp;
+    if (*f_w_c == 0) fp = fopen("dane.txt", "w");
+    else fp = fopen("dane.txt", "a");
+    if (fp == NULL) printf("B£¥D OTWARCIA PLIKU\n");
+    else
+    {
+        if (mode == 0)
+        {
+            printf("\nZapisywanie danych...\n");
+
+            fprintf(fp, "WCZYTYWANIE NR: %hu\n", *f_d_c+1);
+            fprintf(fp, "---------------------\n");
+            fprintf(fp, "DANE WCZYTANE: \n");
+            for (int i = 0; i < n; i++)
+                fprintf(fp ,"%d ", p[i]);
+            fprintf(fp, "\n\n");
+            *f_d_c += 1;
+        }
+        else if(mode == 1 || mode == 2)
+        {
+            printf("\nZapisywanie wyników...\n");
+
+            fprintf(fp, "Posortowane b¹belkowo: \n");
+            for (int i = 0; i < n; i++)
+                fprintf(fp, "%d ", p[i]);
+            fprintf(fp, "\n\n");
+
+            fprintf(fp, "Posortowane quicksort: \n");
+            for (int i = 0; i < n; i++)
+                fprintf(fp, "%d ", q[i]);
+            fprintf(fp, "\n");
+
+            if (mode == 2)
+            {
+                fprintf(fp, "\nŒr. prêdkoœæ algorytmu b¹belkowego: %lf ms\n", avg_b);
+                fprintf(fp, "Œr. prêdkoœæ algorytmu Quicksort: %lf ms\n\n", avg_q);
+                if (avg_b - avg_q > 0.0)
+                    fprintf(fp, "Ró¿nica wynosi %lf ms na korzyœæ sortowania Quicksort", avg_b - avg_q);
+                else if (avg_b - avg_q < 0.0)
+                    fprintf(fp, "Ró¿nica wynosi %lf ms na korzyœæ sortowania b¹belkowego", (avg_b - avg_q) * -1);
+                else
+                    fprintf(fp, "Algorytmy osi¹gnê³y takie same czasy");
+            }
+
+            fprintf(fp, "\n+============================+\n\n");
+        }
+        *f_w_c += 1;
+        fclose(fp);
+    }
+}
 // ---------------------------------------------------
 void display_sorted(int n, int* p, int* q)
 {
@@ -160,20 +213,23 @@ int *allocate_memory(int n)
 
 int main()
 {
-    system("CHCP 1250 >NUL"); //polskie znaki w konsoli
+    system("CHCP 1250 >NUL");
     system("mode 150,40");
 
-    unsigned short mode; //? 1XX-rêczny, 2XX-generowanie, X0X-bez_pliku, X1X-z_plikiem, XX0-bez_czasu, XX1-z_czasem, 777 - exit
-    int quantity; //iloœæ liczb
+    unsigned short mode;
+    int quantity;
+    
     int big_min_max;
     unsigned int iterations;
-
     double avg_bubble;
     double avg_quick;
 
+    unsigned short file_writes_count = 0;
+    unsigned short file_data_load_count = 0;
 
     int* p = NULL;
     int* q = NULL;
+
 
 
     do
@@ -200,16 +256,7 @@ int main()
         {
             menu_header();
             // wszsytko
-        }
-        else if (mode == 210)
-        {
-            menu_header();
-            // plik
-        }
-        else if (mode == 201)
-        {
-            menu_header();
-            printf("AUTOMATYCZNE GENEROWANIE LICZB\n");
+            printf("AUTOMATYCZNE GENEROWANIE LICZB (zapis do pliku i testy wydajnoœci)\n");
             printf("------------------------------\n\n");
             load_quantity(&quantity);
             p = allocate_memory(quantity);
@@ -217,7 +264,47 @@ int main()
 
             big_min_max = auto_data_load(quantity, p, q);
             load_iterations(&iterations);
-            printf("\nTrwa sortowanie...\n\n");
+            file_handling(quantity, p, q, 0, &file_writes_count, &file_data_load_count, 0.0, 0.0);
+            printf("\nTrwa sortowanie...\n");
+            stress_test(quantity, p, q, iterations, &avg_bubble, &avg_quick);
+            if (quantity <= 200 && !big_min_max)
+                display_sorted(quantity, p, q);
+            printf("\nŒr. prêdkoœæ algorytmu b¹belkowego: %lf ms\n", avg_bubble);
+            printf("Œr. prêdkoœæ algorytmu Quicksort: %lf ms\n\n", avg_quick);
+            compare_times(avg_bubble, avg_quick);
+            file_handling(quantity, p, q, 2, &file_writes_count, &file_data_load_count, avg_bubble, avg_quick);
+            free(p);
+            free(q);
+        }
+        else if (mode == 210)
+        {
+            menu_header();
+            printf("AUTOMATYCZNE GENEROWANIE LICZB (zapis do pliku)\n");
+            printf("------------------------------\n\n");
+            load_quantity(&quantity);
+            p = allocate_memory(quantity);
+            q = allocate_memory(quantity);
+            auto_data_load(quantity, p, q);
+            file_handling(quantity, p, q, 0, &file_writes_count, &file_data_load_count, 0.0, 0.0);
+            printf("\nTrwa sortowanie...\n");
+            bubblesort(p, quantity);
+            quicksort(q, 0, quantity - 1);
+            file_handling(quantity, p, q, 1, &file_writes_count, &file_data_load_count, 0.0, 0.0);
+            free(p);
+            free(q);
+        }
+        else if (mode == 201)
+        {
+            menu_header();
+            printf("AUTOMATYCZNE GENEROWANIE LICZB (testy wydajnoœci)\n");
+            printf("------------------------------\n\n");
+            load_quantity(&quantity);
+            p = allocate_memory(quantity);
+            q = allocate_memory(quantity);
+
+            big_min_max = auto_data_load(quantity, p, q);
+            load_iterations(&iterations);
+            printf("\nTrwa sortowanie...\n");
             stress_test(quantity, p, q, iterations, &avg_bubble, &avg_quick);
             if(quantity <= 200 && !big_min_max)
                 display_sorted(quantity, p, q);
